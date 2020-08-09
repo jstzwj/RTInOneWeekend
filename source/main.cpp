49c0b4ed936606fbddc383e0a2a836688794e4b2
Jun Wang <1103870790@qq.com>
2020-08-09 14:29:54 +0800

    draw normal vectors

--- source/main.cpp
+++ source/main.cpp
@@ -1,31 +1,73 @@
 #include <iostream>
 #include <vector>
 #include <cstdint>
+#include <memory>
+#include <limits>
+
 
 #include <vec3.h>
+#include <ray.h>
 #include <image.h>
+#include <hittable.h>
+#include <sphere.h>
+#include <hittable_list.h>
+
+color ray_color(const ray &r, const hittable& world)
+{
+    hit_record rec;
+    if (world.hit(r, 0, std::numeric_limits<double>::infinity(), rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 int main()
 {
-    const int width = 600;
-    const int height = 400;
-
     std::cout << "start rendering" << std::endl;
 
-    Image image(height, width);
-    for (int j = height - 1; j >= 0; --j) {
-        for (int i = 0; i < width; ++i) {
-            auto r = static_cast<float>(i) / (width - 1);
-            auto g = static_cast<float>(j) / (height - 1);
-            auto b = 0.f;
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
-            image.set_color(j, i, color(r, g, b));
+    Image image(image_height, image_width);
+
+    // world
+    hittable_list world;
+    world.add(std::make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(std::make_shared<sphere>(point3(0,-100.5,-1), 100));
+
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+    // render
+
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        for (int i = 0; i < image_width; ++i)
+        {
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r, world);
+
+            image.set_color(j, i, pixel_color);
         }
     }
 
     image.write_to_file("out.bmp");
 
     std::cout << "finish rendering" << std::endl;
-    
+
     return 0;
 }
